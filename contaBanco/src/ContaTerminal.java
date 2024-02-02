741bfb0e51a766d98f17fea265140a40d4f1ebdd
AurélioMSFilho <aureliofilhotj@gmail.com>
2024-02-01 21:04:20 -0300

    Desenvolvendo Desafio

--- contaBanco/src/ContaTerminal.java
+++ contaBanco/src/ContaTerminal.java
@@ -1,7 +1,14 @@
 
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
+        //TODO: Conhecer e import a classe Scanner.
+        //Exibir as mensagens para o nosso Usuário
+        //Obter pela classe Scanner os valores digitados no terminal
+        //Exibir a mensagem da conta criada.
 
-        System.out.println("Hello world!");
+
+
+
+
     }
 }
